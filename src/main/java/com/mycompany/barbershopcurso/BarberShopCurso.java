3f288ef0fd06fe9546aeabe7485009fbb22fbdb9
pauloh48 <paulohsl48@gmail.com>
2022-12-04 22:23:52 -0300

    teste modificacao

--- src/main/java/com/mycompany/barbershopcurso/BarberShopCurso.java
+++ src/main/java/com/mycompany/barbershopcurso/BarberShopCurso.java
@@ -12,6 +12,6 @@ package com.mycompany.barbershopcurso;
 public class BarberShopCurso {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!");
     }
 }
